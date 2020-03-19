dfd65a203612ecfe531919abb794844431b9ebd1
Mayuri Lashkare <49586246+mayuri-9@users.noreply.github.com>
2020-03-20 00:37:12 +0530

    Add files via upload

---
+++ app.R
@@ -0,0 +1,118 @@
+#Load packages
+library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+#Call source file helper functions
+source('helper.R')
+
+#Create the title
+titledata <- tags$label(tags$img(src = "titanic.jpg", height = '60', width = '65'),
+                        tags$b(" Titanic Data Visualization"))
+
+#user interface
+ui <- fluidPage(
+  
+  tags$head(
+    tags$style(HTML("
+                    @import url('my-font.url');
+                    .col-sm-6 {
+                    width:100%;
+                    }
+                    #run{
+                    background-color:green;
+                    color:white;
+                    }
+
+                    .h3, h3{
+                    font-weight: 500;
+                    line-height: 1.5;
+                    color: darkslateblue;
+                    font-family: none;
+                    font-size: 19px;
+                    }
+
+                    .nav-tabs>li.active>a, 
+                      .nav-tabs>li.active>a:focus
+                    , .nav-tabs>li.active>a:hover {
+                      background-color: #dbf9ff;
+                    }
+
+                    .tab-content>.active
+                    {font-family: none;}
+                    "
+    ))),
+  
+  fluidRow(style="color: aliceblue;
+    background-color: midnightblue;",
+           
+           column(6,offset = 3,
+                  titlePanel(titledata)),
+           column(6)
+  ),
+  
+  fluidRow(
+    br()
+  ),
+  
+  fluidRow(
+    column(12,
+           tabBox(
+             
+             # The id lets us use input$tabset1 on the server to find the current tab
+             id = "tabset1",
+             tabPanel("Home", 
+                     sidebarLayout(
+                       sidebarPanel(
+                          helpText("Create graphs with 
+                                   information from the Titanic dataset."),
+                          
+                          selectInput("var", 
+                                      label = "Choose a variable to display",
+                                      choices = c("Port of Embarkation", "Family Size",
+                                                  "Gender","Ticket Class"),
+                                      selected = "Gender"),
+                          
+                          sliderInput("range", 
+                                      label = "Age in years:",
+                                      min = 0, max = 80, value = c(0, 80))
+                          ,
+                        
+                          submitButton("Run Analysis", icon("play"))),
+                        
+                        mainPanel(plotOutput("graph"))
+                      
+             )),
+            # tabPanel("Mode 2", "Tab content 2",tags$style(HTML('.content-wrapper {background-color:blue;}'))),
+             tabPanel("Information", h3("The sinking of the Titanic is one of the most infamous shipwrecks in history.
+
+                      On April 15, 1912, during her maiden voyage, the widely considered “unsinkable” RMS Titanic sank after colliding with an iceberg. Unfortunately, there weren’t enough lifeboats for everyone onboard, resulting in the death of 1502 out of 2224 passengers and crew.
+                      
+                      While there was some element of luck involved in surviving, it seems some groups of people were more likely to survive than others."))
+    
+  
+))))
+
+server <- function(input, output) {
+  output$graph <- renderPlot({
+
+  data <- switch(input$var,
+                   "Port of Embarkation" = 'embark',
+                    "Gender" = 'sex',
+                    "Ticket Class" = 'pc',
+                   "Family Size" = 'fs')
+  if (data == 'embark')
+  {embark_plot(input$range[1], input$range[2])}
+  else if (data == 'sex')
+  {gender_plot(input$range[1], input$range[2])}
+  else if(data == 'pc')
+  {pclass_plot(input$range[1], input$range[2])}
+  else
+  {fsize_plot(input$range[1], input$range[2])}
+  
+  })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
---
+++ helper.R
@@ -0,0 +1,67 @@
+library('ggplot2')
+library('ggthemes')
+library('dplyr')
+
+#read the data
+train <- read.csv('data/train.csv', stringsAsFactors = F)
+train$Fsize <- train$SibSp + train$Parch + 1
+train$Embarked[c(62, 830)] <- 'C'
+train$Age<-as.numeric(as.character(train$Age))
+train$Age[is.na(train$Age)] = median(train$Age, na.rm=TRUE)
+train$Age <- as.integer(train$Age)
+train$Survived[train$Survived == 0] <- 'No'
+train$Survived[train$Survived == 1] <- 'Yes'
+
+#plotting embarked
+embark_plot <- function(n1,n2){
+  n_train <- train
+  n_train$Age <- as.integer(n_train$Age)
+  n_train$Fsize <- n_train$SibSp + n_train$Parch + 1
+  ggplot(data = n_train %>% filter(n_train$Age > n1 & n_train$Age < n2),
+         aes(x = factor(Embarked), fill = factor(Survived))) +
+    geom_bar(stat='count') +
+    theme_few() +
+    labs(x = "Embarked", fill = "Survived")+
+    scale_x_discrete(limit = c("C", "Q", "S"),
+                     labels = c("Cherbourg","Queenstown","Southampton"))
+}
+
+#plotting family size
+fsize_plot <-  function(n1,n2){
+  n_train <- train
+  n_train$Fare <- as.integer(n_train$Fare)
+  n_train$Fsize <- n_train$SibSp + n_train$Parch + 1
+  ggplot(data = n_train %>% filter(n_train$Age > n1 & n_train$Age < n2),
+         aes(x = factor(Fsize), fill = factor(Survived))) +
+    geom_bar(stat='count') +
+    theme_few() +
+    labs(x = "Family Size", fill = "Survived")
+}
+
+#plotting gender
+gender_plot <-  function( n1,n2){
+  n_train <- train
+  n_train$Fare <- as.integer(n_train$Fare)
+  n_train$Fsize <- n_train$SibSp + n_train$Parch + 1
+  ggplot(data = n_train %>% filter(n_train$Age > n1 & n_train$Age < n2),
+         aes(x = factor(Sex), fill = factor(Survived))) +
+    geom_bar(stat='count') +
+    theme_few() +
+    labs(x = "Gender", fill = "Survived")
+}
+
+#plotting Pclass
+pclass_plot <-  function( n1,n2){
+  n_train <- train
+  n_train$Fare <- as.integer(n_train$Fare)
+  n_train$Fsize <- n_train$SibSp + n_train$Parch + 1
+  ggplot(data = n_train %>% filter(n_train$Age > n1 & n_train$Age < n2),
+         aes(x = factor(Pclass), fill = factor(Survived))) +
+    geom_bar(stat='count') +
+    theme_few()+
+    labs(x = "Ticket Class", fill = "Survived")+
+    scale_x_discrete(limit = c("1", "2", "3"),
+                     labels = c("1st","2nd","3rd"))
+}
+
+#embark_plot(40,50)
